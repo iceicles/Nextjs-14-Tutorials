b7d97ddcd4d114adab3c2c3e83f0c8fbfd8d8920
oshiogifted <oshiogifted2011@gmail.com>
2024-09-24 23:36:28 -0400

    handling POST request

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,5 +1,22 @@
 import { comments } from "./data"
 
+// Note: using extension Thunder Client to test requests - similar to postman
+
 export async function GET() {
   return Response.json(comments)
+}
+
+export async function POST(request: Request) {
+  const comment = await request.json()
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text
+  }
+  comments.push(newComment)
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  })
 }
